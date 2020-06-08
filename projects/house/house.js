6bcc6068a32a50dc42878d72f51a03402b4a273a
paulo-rozatto <rozatto.paulo.victor@gmail.com>
2020-06-08 20:41:31 -0300

    added house

--- projects/house/house.js
+++ projects/house/house.js
@@ -2,37 +2,50 @@
 import * as THREE from '../libs/three.module.js';
 import Stats from '../libs/stats.module.js';
 import { PointerLockControls } from '../libs/PointerLockControls.js';
+import { GLTFLoader } from '../libs/GLTFLoader.js';
 
 function main() {
     const stats = initStats();
     const canvas = document.querySelector('#output');
 
     const renderer = new THREE.WebGLRenderer({ canvas });
-    renderer.setClearColor(new THREE.Color(0x2f2f2f));
+    renderer.setClearColor(new THREE.Color(0xefefef));
     renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
 
     const scene = new THREE.Scene();
 
     const camera = new THREE.PerspectiveCamera(45, canvas.clientWidth / canvas.clientHeight, 0.1, 500);
-    camera.position.set(10, 10, 10);
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    camera.position.set(13, 1.8, -2);
     scene.add(camera);
 
-    const light = new THREE.SpotLight(0xfefefe);
-    light.position.set(10, 10, 10);
+    const light = new THREE.DirectionalLight(0xfefefe);
+    light.position.set(50, 80, 50);
     scene.add(light);
 
-    const geo = new THREE.BoxGeometry(3, 3, 3);
-    const mat = new THREE.MeshLambertMaterial({ color: 0xff0000 });
-    const cube = new THREE.Mesh(geo, mat);
-    scene.add(cube);
+    const pointLight = new THREE.PointLight(0xfefefe, 1, 30);
+    scene.add(pointLight);
 
+    const glbLoader = new GLTFLoader();
+    glbLoader.load('../assets/models/round-house.glb',
+        (glb) => {
+            /* indentify meshes wich are part of the ground by looking material name */
+            const isGround = (mesh) => /suc|h1|tile|stone/i.test(mesh.material.name);
+            ground = glb.scene.children[0].children.filter(isGround);
+
+            scene.add(glb.scene);
+        },
+        (xhr) => console.log((xhr.loaded / xhr.total * 100) + '% loaded'),
+        (error) => console.log(error)
+    );
 
     const clock = new THREE.Clock();
     function render() {
         stats.update();
 
         move(clock.getDelta());
+        pointLight.position.x = controls.getObject().position.x;
+        pointLight.position.z = controls.getObject().position.z;
+        pointLight.position.y = controls.getObject().position.y;
 
         renderer.render(scene, camera);
         requestAnimationFrame(render);
@@ -68,10 +81,9 @@ function main() {
     // CAMERA CONTROL CODE
     const controls = new PointerLockControls(camera, renderer.domElement);
 
-    const raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, -1, 0).normalize(), 0, 2);
+    const raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, -1, 0).normalize(), 0, 1.6);
 
-    const ground = [];
-    const ramp = [];
+    let ground = [];
 
     const blocker = document.getElementById('blocker');
     const instructions = document.getElementById('instructions');
@@ -98,7 +110,7 @@ function main() {
 
     scene.add(controls.getObject());
 
-    let speed = 20;
+    let speed = 7;
     let moveForward = false;
     let moveBackward = false;
     let moveLeft = false;
@@ -135,14 +147,11 @@ function main() {
     function move(delta) {
 
         if (!controls.lock) return;
-        let isIntersectingGround = false, isIntersectingRamp = false;
+        let isIntersectingGround = false;
 
         raycaster.ray.origin.copy(controls.getObject().position);
         if (ground.length > 0) {
             isIntersectingGround = raycaster.intersectObjects(ground).length > 0;
-        }
-        if (ramp.length > 0) {
-            isIntersectingRamp = raycaster.intersectObject(ramp).length > 0;
         }
 
         if (moveForward) {
@@ -159,13 +168,13 @@ function main() {
             controls.moveRight(speed * -1 * delta);
         }
 
-        if (moveUp && camera.position.y <= 100) {
+        if (moveUp) {
             camera.position.y += speed * delta;
         }
-        else if (moveDown && !isIntersectingGround && !isIntersectingRamp) {
+        else if (moveDown && !isIntersectingGround) {
             camera.position.y -= speed * delta;
         }
-        else if (isIntersectingRamp) {
+        else if (isIntersectingGround && !moveDown) {
             camera.position.y += speed / 2 * delta;
         }
     }
