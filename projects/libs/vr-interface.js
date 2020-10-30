8852b4016881520d9a1882fcf61d5a917640200f
paulo-rozatto <rozatto.paulo.victor@gmail.com>
2020-10-30 14:57:14 -0300

    gap

--- projects/libs/vr-interface.js
+++ projects/libs/vr-interface.js
@@ -34,6 +34,7 @@ AFRAME.registerComponent('vr-interface', {
     dimension: { type: 'vec2', default: { x: 1, y: 1 } },
     centralize: { type: 'bool', default: true },
     buttonSize: { type: 'vec2', default: { x: 0.30, y: 0.20 } },
+    gap: { type: 'vec2', default: { x: 0.02, y: 0.02 } },
     cursorColor: { type: 'color', default: 'white' },
   },
 
@@ -41,18 +42,18 @@ AFRAME.registerComponent('vr-interface', {
     const self = this;
 
     this.buttons = [];
-    this.camera = document.querySelector('[camera]')
+    this.camera = document.querySelector('[camera]');
 
-    this.cursor = document.createElement('a-entity')
+    this.cursor = document.createElement('a-entity');
 
     this.cursor.setAttribute('cursor', { fuse: true, fuseTimeout: 1000, });
-    this.cursor.setAttribute('raycaster', { far: 1, objects: '.vrInterface-button' })
+    this.cursor.setAttribute('raycaster', { far: 1, objects: '.vrInterface-button' });
     this.cursor.setAttribute('position', { x: 0, y: 0, z: -0.9 });
     this.cursor.setAttribute('geometry', { primitive: 'ring', radiusInner: 0.007, radiusOuter: 0.015 });
     this.cursor.setAttribute('material', { color: this.data.cursorColor, shader: 'flat' });
     this.cursor.setAttribute('animation__click', 'property: scale; startEvents: click; easing: easeInCubic; dur: 150; from: 0.1 0.1 0.1; to: 1 1 1');
-    this.cursor.setAttribute('animation__fusing', 'property: scale; startEvents: fusing; easing: easeInCubic; dur: 1000; from: 1 1 1; to: 0.1 0.1 0.1')
-    this.cursor.setAttribute('animation__fusing2', 'property: scale; startEvents: mouseleave; easing: easeInCubic; dur: 150; to: 1 1 1')
+    this.cursor.setAttribute('animation__fusing', 'property: scale; startEvents: fusing; easing: easeInCubic; dur: 1000; from: 1 1 1; to: 0.1 0.1 0.1');
+    this.cursor.setAttribute('animation__fusing2', 'property: scale; startEvents: mouseleave; easing: easeInCubic; dur: 150; to: 1 1 1');
 
     this.camera.appendChild(this.cursor);
 
@@ -108,13 +109,14 @@ AFRAME.registerComponent('vr-interface', {
 
     button.position.set(
       this.camera.object3D.position.x + data.position.x,
-      (this.camera.object3D.position.y + data.position.y) - i * data.buttonSize.y,
-      (this.camera.object3D.position.z + data.position.z) - j * data.buttonSize.x,
+      (this.camera.object3D.position.y + data.position.y) - i * (data.buttonSize.y + data.gap.y),
+      (this.camera.object3D.position.z + data.position.z) - j * (data.buttonSize.x + data.gap.x),
     );
     if (data.centralize) {
       button.position.y += data.buttonSize.y * 0.5 * (data.dimension.x - 1);
       button.position.z += data.buttonSize.x * 0.5 * (data.dimension.y - 1);
     }
+
     button.onClick = callback;
 
     this.buttons.push(button);
