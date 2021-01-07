105048133fb6b333bd1c39833f4e1e0241a38b37
paulo-rozatto <rozatto.paulo.victor@gmail.com>
2021-01-07 09:48:32 -0300

    testing 4:3

--- projects/libs/vr-interface.js
+++ projects/libs/vr-interface.js
@@ -271,7 +271,11 @@ AFRAME.registerComponent('vr-interface', {
       self.isToChangeRho = true;
 
       self.stopButton.object3D.visible = true;
-      self.stopButton.object3D.position.set((data.dimension.y / 2 * data.buttonSize.x + 0.06), (-data.dimension.x * data.buttonSize.y) / 4 - 0.05, 0.01);
+      self.stopButton.object3D.position.set(
+        (data.dimension.y / 2 * data.buttonSize.x + 0.06),
+        (-data.dimension.x + 1) * data.buttonSize.y / 2,
+        0.01
+      );
       self.stopButton.object3D.rotation.z = 0;
       self.stopButton.classList.add('vrInterface-button');
     }
