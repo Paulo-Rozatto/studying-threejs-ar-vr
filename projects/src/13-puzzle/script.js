7de9662c6e8ddbc0645a434ef9397d206547b6c1
Paulo Rozatto <rozatto.paulo.victot@gmail.com>
2022-05-09 23:23:10 -0300

    finished 2d prototype

--- projects/src/13-puzzle/script.js
+++ projects/src/13-puzzle/script.js
@@ -7,6 +7,14 @@ let orbi;
 
 let tile, puzzle;
 
+let state = 0;
+const STATES = [
+    { left: 0, down: 0, right: 0, up: 1 },
+    { left: 0, down: -1, right: 1, up: 0 },
+    { left: -1, down: 1, right: 0, up: 0 },
+    { left: 0, down: 0, right: 0, up: -1 },
+]
+
 init();
 animate();
 
@@ -63,27 +71,28 @@ function init() {
     orbi = new Orbi(camera, config);
     cameraHolder.add(orbi);
 
-    orbi.addButton('1', '../07-testing-room/img/action1.png', () => {
-        orbi.showMessage('button 1')
+    orbi.addButton('1', 'img/up.png', () => {
+        move('up')
     });
-    orbi.addButton('2', '../07-testing-room/img/action2.png', () => {
-        orbi.showMessage('button 2')
+    orbi.addButton('2', 'img/down.png', () => {
+        move('down')
     });
-    orbi.addButton('3', '../07-testing-room/img/action3.png', () => {
-        orbi.showMessage('button 3')
+    orbi.addButton('3', 'img/left.png', () => {
+        move('left')
     });
-    orbi.addButton('4', '../07-testing-room/img/action4.png', () => {
-        orbi.showMessage('button 4')
+    orbi.addButton('4', 'img/right.png', () => {
+        move('right')
     });
 
     const tileGeo = new THREE.PlaneBufferGeometry(0.1, .1);
-    const tileMat = new THREE.MeshBasicMaterial({ color: 0x770000 })
-    const tile = new THREE.Mesh(tileGeo, tileMat);
-    tile.position.set(0.15, 1.6, -1);
+    const tileMat = new THREE.MeshBasicMaterial({ color: 0x770000 });
+    tile = new THREE.Mesh(tileGeo, tileMat);
+    tile.position.set(0.115, 1.425, -1);
     scene.add(tile);
 
+    const puzzTex = new THREE.TextureLoader().load("img/puzzle1.png");
     const puzzleGeo = new THREE.PlaneBufferGeometry(0.5, 0.5);
-    const puzzleMat = new THREE.MeshBasicMaterial({ color: 0x007700 })
+    const puzzleMat = new THREE.MeshBasicMaterial({ map: puzzTex });
     puzzle = new THREE.Mesh(puzzleGeo, puzzleMat);
     puzzle.position.set(0.3, 1.6, -1.1);
     scene.add(puzzle);
@@ -98,4 +107,31 @@ function animate() {
 function render() {
     orbi.update();
     renderer.render(scene, camera);
+}
+
+function move(dir) {
+    let step = STATES[state][dir] || 0;
+
+    if (step != 0) {
+        state += step;
+
+        switch (dir) {
+            case 'up':
+                tile.position.y += 0.2;
+                break;
+            case 'right':
+                tile.position.x += 0.31;
+                break;
+            case 'down':
+                tile.position.y -= 0.2;
+                break;
+            case 'left':
+                tile.position.x -= 0.31;
+                break;
+        }
+
+        if(state === 3 ) {
+            orbi.showMessage('Terminou!')
+        }
+    }
 }
